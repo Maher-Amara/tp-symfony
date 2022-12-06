7183b5f646f88f371b372e20252aaacf0be88d82
maher amara <maher.amara@ETUDE.LOCAL>
2022-12-06 18:11:37 +0100

    create entity condidature

---
+++ src/Entity/Candidature.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CandidatureRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CandidatureRepository::class)]
+class Candidature
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Job $Job = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getJob(): ?Job
+    {
+        return $this->Job;
+    }
+
+    public function setJob(?Job $Job): self
+    {
+        $this->Job = $Job;
+
+        return $this;
+    }
+}
