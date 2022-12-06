6858bd41343e3f79f38013c4892b033bb0ecc8a0
maher amara <maher.amara@ETUDE.LOCAL>
2022-12-06 18:26:02 +0100

    create entity condidature

--- src/Entity/Candidature.php
+++ src/Entity/Candidature.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\CandidatureRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CandidatureRepository::class)]
@@ -17,6 +18,15 @@ class Candidature
     #[ORM\JoinColumn(nullable: false)]
     private ?Job $Job = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $Candidat = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $Contenu = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -30,6 +40,42 @@ class Candidature
     public function setJob(?Job $Job): self
     {
         $this->Job = $Job;
+
+        return $this;
+    }
+
+    public function getCandidat(): ?string
+    {
+        return $this->Candidat;
+    }
+
+    public function setCandidat(string $Candidat): self
+    {
+        $this->Candidat = $Candidat;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->Contenu;
+    }
+
+    public function setContenu(string $Contenu): self
+    {
+        $this->Contenu = $Contenu;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
 
         return $this;
     }
